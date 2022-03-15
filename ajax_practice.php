fbd396554e328fef149d73772a2b7408047f5c81
t.ogawa <97233360+tonagawa@users.noreply.github.com>
2022-03-16 02:27:45 +0900

    動作修正版

---
+++ ajax_practice.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRACTICE</title>
+    <script src="./js/jquery-3.6.0/jquery-3.6.0.js"></script>
+    <script src="js/new_ajax_practice.js"></script>
+</head>
+<body>
+    <header>
+        <p><a href="../index.html">ホームメニューへ戻る</a></p>
+    </header>
+
+    <main>
+        <?php
+            require 'new_main.php';
+        ?>
+    </main>
+</body>
+</html>
